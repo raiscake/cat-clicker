6143de6acdf1a3c27a6a332407904b4b1701984b
Raisa Yang <raisayangc9@gmail.com>
2018-11-19 22:19:28 -0600

    Add click functionality for both cats

--- js/app.js
+++ js/app.js
@@ -3,14 +3,16 @@ const xuxa = {
     name: 'Xuxa',
     id: 'cat1',
     image: 'cat1.jpg',
-    count: 0
+    count: 0,
+    imgID: 'cat1-image'
 };
 
 const halfie = {
     name: 'Halfie',
     id: 'cat2',
     image: 'cat2.jpg',
-    count: 0
+    count: 0,
+    imgID: 'cat2-image'
 };
 
 // Put cats into an array
@@ -19,8 +21,8 @@ const allCats = [xuxa, halfie];
 // Place cat divs
 function placeDiv(cat) {
     const template = `
-    <div class="cat">
-        <img src="img/${cat.image}" alt="" class="cat-image" id="${cat.id}">
+    <div class="cat" id="${cat.id}">
+        <img src="img/${cat.image}" alt="" class="cat-image" id="${cat.id}-image">
         <p>Times clicked: <span id="${cat.id}-click-total">0</span></p>
     </div>`;
 
@@ -30,17 +32,27 @@ function placeDiv(cat) {
     body.appendChild(div);
 }
 
-for(let cat of allCats) {
-    placeDiv(cat);
-    console.log(cat.id);
-}
 
+for(let i = 0; i < allCats.length; i++) {
+    let cat = allCats[i];
+    placeDiv(cat);
+}
 
 // Count clicks
 function countClicks(cat) {
-    var clickCounter = document.getElementById(cat.id);
-    count++;
-    clickCounter.textContent = count;
+    let clickID = `${cat.id}-click-total`,
+        clickCounter = document.getElementById(clickID);
+    cat.count++;
+    clickCounter.textContent = cat.count;
 }
 
-// kitten.addEventListener('click', countClicks);
+const xuxaDiv = document.getElementById(xuxa.imgID),
+    halfieDiv = document.getElementById(halfie.imgID);
+
+xuxaDiv.addEventListener('click', function() {
+    countClicks(xuxa);
+})
+
+halfieDiv.addEventListener('click', function() {
+    countClicks(halfie);
+});
